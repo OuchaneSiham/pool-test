5d5cbad055383a75d0f64ecfcd71853fb75d70d8
siham <ouchanesiham42@gmail.com>
2023-08-06 04:41:04 +0200

    ex05

--- c00/ex05/ft_print_comb.c
+++ c00/ex05/ft_print_comb.c
@@ -5,6 +5,17 @@ void ft_putchar(char c)
 	write(1, &c, 1);
 }	
 
+void	ft_siham(int i, int j, int k)
+{
+	 ft_putchar(i);
+         ft_putchar(j);
+         ft_putchar(k);
+         if (i!='7')
+	 {
+         	ft_putchar(',');
+                ft_putchar(' ');
+	}
+}
 void ft_print_comb(void)
 {
 	char	i;
@@ -19,13 +30,7 @@ void ft_print_comb(void)
 			k = j + 1;
 			while (k <= '9')
 			{
-				ft_putchar(i);
-				ft_putchar(j);
-				ft_putchar(k);
-				if (i!='7'){
-					ft_putchar(',');
-					ft_putchar(' ');
-				}
+				ft_siham(i, j, k);
 				k++;
 			}
 			j++;
